6607f0f0b4eff1ad5a311cd277aac7fff6d47610
Esben Højris <esbh@itu.dk>
2021-06-11 14:51:51 +0200

    Add more tests

--- ForParse.Tests/Tests.fs
+++ ForParse.Tests/Tests.fs
@@ -5,8 +5,12 @@ open Xunit
 
 open ForParse
 
-[<Fact>]
-let ``Parse A`` () =
-    let exp = (true, "BC")
-    let act = Parser.parseA "ABC"
-    Assert.Equal (exp, act)
+[<Theory>]
+[<InlineData("ABC", true, "BC")>]
+[<InlineData("BC", false, "BC")>]
+[<InlineData("A", true, "")>]
+[<InlineData("", false, "")>]
+[<InlineData("AAAAC", true, "AAAC")>]
+let ``Parse A`` (inp, exp1, exp2) =
+    let act = Parser.parseA inp
+    Assert.Equal ((exp1, exp2), act)
