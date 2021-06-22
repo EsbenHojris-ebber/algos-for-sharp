ddc0804a8ad351ab218deaff426d699f814f8126
Esben Højris <esbh@itu.dk>
2021-06-22 12:40:44 +0200

    Add JNumber parser

--- ForParse/JSONParser.fs
+++ ForParse/JSONParser.fs
@@ -72,3 +72,54 @@ let jString =
     quotedString
     |>> JString
     <?> "string"
+
+let JNumber =
+    let optSign = pchar '-' |> opt
+
+    let zero = pstring "0"
+
+    let digitOneNine = satisfy (fun c -> System.Char.IsDigit c && c <> '0') "digit"
+
+    let point = pchar '.'
+
+    let e = pchar 'e' <|> pchar 'e'
+
+    let optPlusMinus = pchar '+' <|> pchar '-'
+
+    let nonZeroInt = 
+        digitOneNine
+        .>>. manyChars digit
+        |>> fun (first, rest) -> string first + rest
+
+    let intPart = zero <|> nonZeroInt
+
+    let fractionPart = point >>. manyChars1 digit
+
+    let exponentPart = e >>. optSign .>>. manyChars1 digit
+
+    let ( |>? ) opt f =
+        match opt with
+        | None -> ""
+        | Some x -> f x
+
+    let convertToJNumber (((optSign, intPart), fractionPart), exponentPart) =
+        let signStr = optSign |>? string
+
+        let fractionPartStr =
+            fractionPart 
+            |>? fun digits -> "." + digits
+
+        let exponentPartStr =
+            exponentPart
+            |>? (fun (optSign, digits) -> 
+                let sign = optSign |>? string
+                "e" + sign + digits)
+
+        (signStr + intPart + fractionPartStr + exponentPartStr)
+        |> float
+        |> JNumber
+
+    optSign .>>. intPart .>>. opt fractionPart .>>. opt exponentPart
+    |>> convertToJNumber
+    .>> spaces1
+    <?> "number"
